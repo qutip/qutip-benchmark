6275290d49f4e947be55398de076ab286459aae2
Xavier Spronken <xavier.spronken@gmail.com>
2022-08-25 00:14:18 +0200

    add type and model param filters

--- view_nightly_benchmarks.py
+++ view_nightly_benchmarks.py
@@ -377,7 +377,21 @@ def main(args=[]):
                         help="""Specify which parameter should be used
                         to separate line on the plot instead of separating the
                         plots. default=None, ['type', 'model'] if nothing
-                        specified""")
+                        specified
+                        WARNING: If a parameter is used as line_sep it cannot
+                        be used as a filter""")
+    parser.add_argument('--dtype', nargs="+", default=None, type=str,
+                        help="""Specify which dtypes to plot, plots all by
+                        default or if nothing specified
+                        Only applies to linear algebra benchmarks""")
+    parser.add_argument('--model', nargs="+", default=None, type=str,
+                        help="""Specify which models to plot, plots all by
+                        default or if nothing specified
+                        Only applies to solver benchmarks""")
+    parser.add_argument('--coeftype', nargs="+", default=None, type=str,
+                        help="""Specify which coefficient types to plot, plots
+                        all by default or if nothing specified
+                        Only applies to QobjEvo benchmarks""")
 
     args = parser.parse_args()
     param_filters = None
@@ -390,12 +404,19 @@ def main(args=[]):
         args.line_sep = ['type', 'model']
 
     # create param_filter dict if called
-    if args.density or args.size:
+    if (args.density or args.size or args.model
+             or args.dtype or args.coeftype):
         param_filters = {}
         if args.density:
             param_filters["density"] = args.density
         if args.size:
             param_filters["size"] = args.size
+        if args.dtype:
+            param_filters["dtype"] = args.dtype
+        if args.model:
+            param_filters["model"] = args.model
+        if args.coeftype:
+            param_filters["coeftype"] = args.coeftype
 
     if args.line_sep:
         line_sep = args.line_sep
