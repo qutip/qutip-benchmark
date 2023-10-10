c9aab8273a41dd7a35bc7fc63103cdee7ac49b10
Eric Giguere <eric.giguere@gmail.com>
2023-10-10 12:06:07 -0400

    lint

--- qutip_benchmark/benchmarks/bench_solvers.py
+++ qutip_benchmark/benchmarks/bench_solvers.py
@@ -169,7 +169,7 @@ def bench_steadystate(benchmark, model_steady, size):
     benchmark.group = "solvers:steadystate"
 
     if model_steady == "Cavity":
-        pytest.skipif(size>=128, "Too slow")
+        pytest.skipif(size >= 128, "Too slow")
         H, _, c_ops, _ = cavity_setup(size)
 
     elif model_steady == "Jaynes-Cummings":
