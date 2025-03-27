bcfb31ef08a5e66d226bb4e09cae5b9979935900
Maximilian Meyer-Mölleringhof <mixmax199989@gmail.com>
2025-03-27 18:53:19 +0800

    Hooks for package version collection

--- benchmark/qutip/runbenchmark.py
+++ benchmark/qutip/runbenchmark.py
@@ -1,7 +1,5 @@
 import sys
 import pytest
-import glob
-import json
 from importlib.metadata import version, PackageNotFoundError
 
 def pkg_version(package):
@@ -10,37 +8,13 @@ def pkg_version(package):
     except PackageNotFoundError:
         return None
 
-
-def get_latest_benchmark():
-    """Returns the path to the latest benchmark run from `./benchmark/results/qutip`"""
-
-    benchmark_paths = glob.glob("./benchmark/results/qutip/*/*.json")
-    dates = [
-        "".join(_b.split("/")[-1].split("_")[2:4]) for _b in benchmark_paths
-    ]
-    benchmarks = {date: value for date, value in zip(dates, benchmark_paths)}
-
-    dates.sort()
-    latest = dates[-1]
-    benchmark_latest = benchmarks[latest]
-
-    return benchmark_latest
-
-
-def add_packages_to_json(filepath):
-    """Loads the created JSON file and adds package versions"""
-
-    with open(filepath, "r") as f:
-        data = json.load(f)
-        data["package_versions"] = {}
-        data["package_versions"]["scipy"] = pkg_version("scipy")
-        data["package_versions"]["numpy"] = pkg_version("numpy")
-        data["package_versions"]["cython"] = pkg_version("cython")
-        data["package_versions"]["qutip"] = pkg_version("qutip")
-        data["package_versions"]["pytest"] = pkg_version("pytest")
-    with open(filepath, "w") as f:
-        json.dump(data, f, indent=4, separators=(",", ": "))
-    return data
+@pytest.hookimpl()
+def pytest_benchmark_update_json(config, benchmarks, output_json):
+    output_json["package_versions"]["scipy"] = pkg_version("scipy")
+    output_json["package_versions"]["numpy"] = pkg_version("numpy")
+    output_json["package_versions"]["cython"] = pkg_version("cython")
+    output_json["package_versions"]["qutip"] = pkg_version("qutip")
+    output_json["package_versions"]["pytest"] = pkg_version("pytest")
 
 def run_benchmarks():
     return pytest.main(
@@ -57,10 +31,7 @@ def run_benchmarks():
     )
 
 def main():
-    exit_code = run_benchmarks()
-    benchmark_latest = get_latest_benchmark()
-    add_packages_to_json(benchmark_latest)
-    return exit_code
+    return run_benchmarks()
 
 
 if __name__ == "__main__":
