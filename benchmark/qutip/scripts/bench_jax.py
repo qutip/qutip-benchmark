60983ed73f38a4e9831d110c4804a58f57baa0d4
Vishwas <vishwasbajaj.global@gmail.com>
2026-02-25 00:38:15 +0530

    Refactor spacing and formatting in bench_jax.py

--- benchmark/qutip/scripts/bench_jax.py
+++ benchmark/qutip/scripts/bench_jax.py
@@ -1,10 +1,9 @@
 import pytest
-pytest.importorskip("jax")
-pytest.importorskip("jaxlib")
 import jax
 import jax.numpy as jnp
 import qutip_jax as qj
 from qutip import mesolve, basis, sigmax, sigmaz, CoreOptions
+
 
 @pytest.mark.jax
 def bench_jax_mesolve(benchmark):
@@ -12,14 +11,15 @@ def bench_jax_mesolve(benchmark):
     qj.set_as_default()
 
     with jax.default_device(jax.devices("gpu")[0]):
-        opt = { "method": "diffrax", "normalize_output": False }
+        opt = {"method": "diffrax", "normalize_output": False}
 
         with CoreOptions(default_dtype="jax"):
             delta = jnp.pi
             g = 0.2
-            H = delta/2.0 * sigmax()
+            H = delta / 2.0 * sigmax()
             c_ops = [jnp.sqrt(g) * sigmaz()]
             e_ops = [sigmaz()]
             psi0 = basis(2, 0)
             tlist = jnp.linspace(0, 10, 100)
+
             benchmark(mesolve, H, psi0, tlist, c_ops, e_ops=e_ops, options=opt)
